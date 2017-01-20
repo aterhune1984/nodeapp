2cdc42b34fa8bee83a66702e88fee6ef61419e4d
andy.terhune <andy.terhune@rackspace.com>
2017-01-20 13:14:11 -0600

    changing appspec

--- Scripts/installdeps.sh
+++ Scripts/installdeps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-. ~/.nvm/nvm.sh
+source ~/.nvm/nvm.sh
 nvm install stable
 node -e "console.log('Running Node.js ' + process.version)"
 npm install
